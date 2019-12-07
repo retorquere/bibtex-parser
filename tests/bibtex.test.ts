dcfa584461a947ac2de3f82d2d8906525059c684
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-12-07 13:39:07 +0100

    cleanup

--- tests/bibtex.test.ts
+++ tests/bibtex.test.ts
@@ -13,6 +13,8 @@ const snaps = path.join(__dirname, '__snapshots__')
 const enable = {
   ast: process.env.AST !== 'false',
   zotero: process.env.ZOTERO !== 'false',
+  case: (process.env.TESTCASE || '').toLowerCase(),
+  big: (process.env.BIG || process.enc.CI === 'true'),
 }
 
 const ignoreErrors = {
@@ -32,7 +34,10 @@ describe('BibTeX Parser', () => {
   let root = path.join(__dirname, 'cases')
   for (const f of fs.readdirSync(root).sort()) {
     if (!f.replace(/(la)?tex$/, '').endsWith('.bib')) continue
+    if (enable.case && !f.toLowerCase().includes(enable.case)) continue
+
     const caseName = path.basename(f)
+
     const input = fs.readFileSync(path.join(root, f), 'utf-8')
     if (enable.ast) {
       it(`should parse ${caseName} to an AST`, () => {
@@ -52,13 +57,14 @@ describe('BibTeX Parser', () => {
     for (const f of fs.readdirSync(root).sort()) {
       if (!f.replace(/(la)?tex$/, '').endsWith('.bib')) continue
 
-      if (!process.env.BIG) {
-        if (f === 'Async import, large library #720.bib') continue
-        if (f === 'Really Big whopping library.bib') continue
-        if (process.env.TEST && !f.includes(process.env.TEST)) continue
+      if (!enable.big) {
+        if (f.includes('Async import, large library #720')) continue
+        if (f.includes('Really Big whopping library.')) continue
+        if (enable.case && !f.toLowerCase().includes(enable.case)) continue
       }
 
       const caseName = `bbt-${mode}-${path.basename(f)}`
+
       const input = fs.readFileSync(path.join(root, f), 'utf-8')
 
       if (enable.ast) {
