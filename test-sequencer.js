c488f5b994ff0fe2f7791b20319d07df51d1172c
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-12-25 14:03:59 +0100

    cleanup

--- test-sequencer.js
+++ test-sequencer.js
@@ -10,7 +10,6 @@ class CustomSequencer extends Sequencer {
       .filter(p => paths.includes(p)) // only use paths from order that are in tests -- should be all
       .concat(paths.filter(p => !order.includes(p))) // use paths not found in order -- should be none
       .map(p => tests.find(t => t.path === p)) // map back to tests
-    console.log(ordered.map(t => t.path))
     return ordered
   }
 }
