237da2f4554be0dca52c15a51b38bcdb4e598129
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-02-16 22:48:05 +0100

    progress

--- unified.ts
+++ unified.ts
@@ -2,10 +2,15 @@ import { unified } from 'unified'
 import { printRaw } from "@unified-latex/unified-latex-util-print-raw"
 import * as chunker from './chunker'
 import { unifiedLatexFromString } from '@unified-latex/unified-latex-util-parse'
-// import { replaceNode } from '@unified-latex/unified-latex-util-replace'
+import { replaceNode } from '@unified-latex/unified-latex-util-replace'
 import { parseLigatures } from '@unified-latex/unified-latex-util-ligatures'
+import { latex2unicode, combining } from 'unicode2latex'
+
+import * as fs from 'fs'
 
 const content = "``Heyns'', \\mbox{Emiliano} and Heyns, Emile, Jr."
+
+function argumentParser
 
 const parser = unified().use(unifiedLatexFromString, {
   macros: {
@@ -54,9 +59,6 @@ const parser = unified().use(unifiedLatexFromString, {
   },
 })
 
-let input = require('./test.json').bib
-input = "@article{x, x={\\em{x}\\href{url\\too}{label}}}"
-const entries = chunker.entries(input).entries
 import { visit } from '@unified-latex/unified-latex-util-visit'
 
 function context(parents) {
@@ -68,6 +70,58 @@ function context(parents) {
 }
 
 function convert(tree) {
+  /*
+  visit(tree, {
+    enter(node, info) {
+      let parent
+      switch (node.type) {
+        case 'group':
+          node.bibtex = { protectCase: true }
+          break
+        case 'argument':
+          node.bibtex = { protectCase: node.openMark === '{' }
+          break
+
+        case 'macro':
+          if ((parent = info.parents[0]) && parent.bibtex && (Array.isArray(parent.content) ? parent.content[0] : parent.content) === node) {
+            delete parent.bibtex.protectCase
+          }
+          break
+      }
+    },
+    leave(node, info) {
+      // console.log('  '.repeat(info.parents.length), 'leave', info, node.type)
+    }
+  })
+  */
+
+  replaceNode(tree, (node, info) => {
+    if (info.parents[0]?.type === 'macro' && info.parents[0].content.match(/^(url|href)$/) && node === info.parents[0].args[0]) {
+      return {
+        type: 'verbatim',
+        env: 'verbatim',
+        content: printRaw(node.content),
+      }
+    }
+
+    // return null to delete
+    if (node.type === 'macro') {
+      let text: string
+      switch (node.content) {
+        case 'frac':
+          if (text = latex2unicode[`\\frac{${printRaw(node.args[0].content)}}{${printRaw(node.args[1].content)}}`]) {
+            return { type: 'string', content: text }
+          }
+          else {
+            return [...node.args[0].content, { type: 'string', contents: '\u2044' }, ...node.args[1].content]
+          }
+      }
+    }
+    else if (node.type === 'string') {
+      if (node.content === '~') return {...node, content: '\u00A0' }
+    }
+  })
+
   visit(tree, (nodes, info) => { // eslint-disable-line @typescript-eslint/no-unsafe-argument
     if (info.context.inMathMode || info.context.hasMathModeAncestor) return
 
@@ -75,22 +129,22 @@ function convert(tree) {
     nodes.length = 0
     nodes.push(...parsed)
   }, { includeArrays: true, test: Array.isArray })
-  
-  visit(tree, (node, info) => { // eslint-disable-line @typescript-eslint/no-unsafe-argument
-    if (node.type === 'macro' && node.content.match(/^(url|href)$/)) {
-      node.args[0].content = [ { type: 'string', content: printRaw(node.args[0].content) } ]
-    }
-  })
-}
 
-/*
-replaceNode(ast, (node) => {
-    // return null to delete
-    if (node.type == 'macro' && node.content == '_' && node.args[0].type == 'string' && node.args[0].content == '1') return {...node, type: 'string', content: '\u2081', args: null }
-    // if (node.type === 'string' && node.content === '~') return {...node, content: '\u00A0' }
-    return undefined
-})
-*/
+  visit(tree, (nodes, info) => { // eslint-disable-line @typescript-eslint/no-unsafe-argument
+    if (nodes.length === 1) return
+    const condensed = []
+    let node
+    while (node = nodes.shift()) {
+      if (!condensed.length || node.type !== 'string' || condensed[condensed.length - 1].type !== 'string') {
+        condensed.push(node)
+      }
+      else {
+        condensed[condensed.length - 1].content += node.content
+      }
+    }
+    nodes.push(...condensed)
+  }, { includeArrays: true, test: Array.isArray })
+}
 
 const verbatim = [
     'doi',
@@ -105,10 +159,15 @@ const verbatim = [
     'verbc',
 ]
 
+let input = fs.readFileSync('test.bib', 'utf-8')
+const entries = chunker.entries(input).entries
+
 for (const entry of entries) {
   for (const [k, v] of Object.entries(entry.fields)) {
     const ast = parser.parse(v)
-    if (!verbatim.includes(k)) convert(ast)
-    // console.log(entry.type, entry.key, k, JSON.stringify(ast, null, 2)) // eslint-disable-line no-console
+    if (!verbatim.includes(k)) {
+      convert(ast)
+      console.log(entry.type, entry.key, k, JSON.stringify(ast, null, 2)) // eslint-disable-line no-console
+    }
   }
 }
