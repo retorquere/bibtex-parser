5eaa04d47e9b7f89fa374e91b42626c55b32ff62
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-04-15 16:57:03 +0200

    sentence caser

--- snap.sh
+++ snap.sh
@@ -3,7 +3,7 @@
 for sc in on+guess on off; do
   for cp in as-needed off strict; do
     for pq in yes no; do
-      ./runtests --sc $sc --cp $cp --pq $pq --snap
+      ./runtests --sc $sc --cp $cp --pq $pq --big --snap
     done
   done
 done
