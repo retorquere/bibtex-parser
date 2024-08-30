60e172c905f5a4292f9e1e7f345af2e637468adf
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-08-30 12:39:44 +0200

    lint

--- console.py
+++ console.py
@@ -3,25 +3,24 @@
 import json
 import sys
 
-with open('.eslintrc.json') as f:
-  eslint = json.load(f)
+console = 'eslint.config.console.json'
 
 if len(sys.argv) == 1:
   pass
 else:
   state, = sys.argv[1:]
   if state in ['on', 'error']:
-    eslint['rules']['no-console'] = state
-  elif state in ['r', 'rel']:
-    eslint['rules']['no-console'] = 'error'
+    pass
+  elif state in ['off', 'r', 'rel']:
+    state = 'error'
   elif state in ['d', 'c', 'deb']:
-    eslint['rules']['no-console'] = 'off'
+    state = 'off'
   else:
     print('unexpected state', state)
     sys.exit(1)
 
-  with open('.eslintrc.json', 'w') as f:
-    json.dump(eslint, f, sort_keys=True, indent='  ')
+  with open(console, 'w') as f:
+    json.dump(state, f)
 
-mode = { "error": "release", "off": "debug" }
-print(eslint['rules']['no-console'], f"({mode[eslint['rules']['no-console']]})")
+with open(console) as f:
+  print(json.load(f))
